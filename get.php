c02a48a5872c5b45571813ad81d997b2259b5c8d
Sid220 <74916637+Sid220@users.noreply.github.com>
2021-06-17 09:47:20 -0400

    Update get.php

--- get.php
+++ get.php
@@ -1,7 +1,7 @@
 <html>
     <body>
 <?php
-$city="CITY";
+$city="<CITY>";
 $country="<2-DIGIT_COUNTRY_CODE>"; 
 $url="http://api.openweathermap.org/data/2.5/weather?lat=<LATITUDE>&lon=<LONGITUDE>&appid=<API_KEY>";
 $json=file_get_contents($url);
